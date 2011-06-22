e1f89ccac12cc15d6d636d3360fba43d3fb13af0
Simon Bartlett <simon.bartlett@me.com>
2011-06-22 23:32:41 +0100

    Fixing whitespace.

--- jquery.ui.lookup.js
+++ jquery.ui.lookup.js
@@ -9,65 +9,65 @@
 
 (function ($) {
 
-    $.lookup = {
+	$.lookup = {
 
-        createBodyTemplate: function (name) {
-            return '<div id="' + name + '" style="display:none;">' +
-                '<div style="height: 30px;"><label style="display: inline;">Search: <input type="text" class="ui-widget-content ui-corner-all" style="padding: 2px;" /></label></div>' +
-                '<div class="ui-lookup-results"></div>' +
-                '</div>';
-        }
+		createBodyTemplate: function (name) {
+			return '<div id="' + name + '" style="display:none;">' +
+				'<div style="height: 30px;"><label style="display: inline;">Search: <input type="text" class="ui-widget-content ui-corner-all" style="padding: 2px;" /></label></div>' +
+				'<div class="ui-lookup-results"></div>' +
+				'</div>';
+		}
 
-    };
+	};
 
-    $.widget("ui.lookup", {
-        version: "0.1.0",
-        options: {
-            cancelText: 'Cancel',
-            delay: 300,
-            disabled: null,
-            height: 400,
-            minLength: 1,
-            modal: 400,
-            name: null,
-            okText: 'Ok',
-            select: null,
-            source: null,
-            title: 'Search',
-            value: null,
-            width: 600
-        },
-        _create: function () {
+	$.widget("ui.lookup", {
+		version: "0.1.0",
+		options: {
+			cancelText: 'Cancel',
+			delay: 300,
+			disabled: null,
+			height: 400,
+			minLength: 1,
+			modal: 400,
+			name: null,
+			okText: 'Ok',
+			select: null,
+			source: null,
+			title: 'Search',
+			value: null,
+			width: 600
+		},
+		_create: function () {
 
-            if (this.options.name === null) {
-                this.options.name = 'dialog_' + Math.floor(Math.random() * 10001).toString();
-            }
+			if (this.options.name === null) {
+				this.options.name = 'dialog_' + Math.floor(Math.random() * 10001).toString();
+			}
 
-            var $this = this,
+			var $this = this,
 				dialogBody = $.lookup.createBodyTemplate($this.options.name),
 				buttons = { };
 
-            buttons[$this.options.okText] = function () {
-                $this.options.value = $this.options._value;
-                if ($this.options.select) {
-                    $this.options.select($this.options.value);
-                }
-                $(this).dialog("close");
-            };
-            buttons[$this.options.cancelText] = function () {
-                $(this).dialog("close");
-            };
+			buttons[$this.options.okText] = function () {
+				$this.options.value = $this.options._value;
+				if ($this.options.select) {
+					$this.options.select($this.options.value);
+				}
+				$(this).dialog("close");
+			};
+			buttons[$this.options.cancelText] = function () {
+				$(this).dialog("close");
+			};
 
-            $this._dialog = $(dialogBody).dialog({
-                autoOpen: false,
-                height: $this.options.height,
-                modal: true,
-                resizable: false,
-                title: $this.options.title,
-                width: $this.options.width,
-                buttons: buttons,
-                open: function (dialogEvent, dialogUi) {
-                    $this._autocomplete = $('input', $this._dialog)
+			$this._dialog = $(dialogBody).dialog({
+				autoOpen: false,
+				height: $this.options.height,
+				modal: true,
+				resizable: false,
+				title: $this.options.title,
+				width: $this.options.width,
+				buttons: buttons,
+				open: function (dialogEvent, dialogUi) {
+					$this._autocomplete = $('input', $this._dialog)
 						.autocomplete({
 							appendTo: '#' + $this.options.name + ' .ui-lookup-results',
 							delay: $this.options.delay,
@@ -107,29 +107,29 @@
 							}
 						})
 						.focus();
-                }
-            });
+				}
+			});
 
-            $this.element.bind('focus', function () {
-                if (!$this.options.disabled) {
-                    $this._dialog.dialog('open');
-                }
-            });
-        },
-        value: function () {
-            return this.options.value;
-        },
-        destroy: function () {
+			$this.element.bind('focus', function () {
+				if (!$this.options.disabled) {
+					$this._dialog.dialog('open');
+				}
+			});
+		},
+		value: function () {
+			return this.options.value;
+		},
+		destroy: function () {
 			this._autocomplete.autocomplete('destroy');
-            this._dialog.dialog('destroy');
-            $.Widget.prototype.destroy.apply(this, arguments);
-        },
-        disable: function () {
-            this.options.disabled = true;
-        },
-        enable: function () {
-            this.options.disabled = false;
-        }
-    });
+			this._dialog.dialog('destroy');
+			$.Widget.prototype.destroy.apply(this, arguments);
+		},
+		disable: function () {
+			this.options.disabled = true;
+		},
+		enable: function () {
+			this.options.disabled = false;
+		}
+	});
 
 }(jQuery));
