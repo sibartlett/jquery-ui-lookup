518e61494cbe57125fba7d4dcbbe0aee87c02e27
Simon Bartlett <simon.bartlett@me.com>
2011-06-22 23:29:49 +0100

    Tidying up, and fixed destroy method.

--- jquery.ui.lookup.js
+++ jquery.ui.lookup.js
@@ -7,7 +7,7 @@
  * https://github.com/SimonBartlett/jquery-ui-lookup
  */
 
-(function($) {
+(function ($) {
 
     $.lookup = {
 
@@ -34,32 +34,31 @@
             select: null,
             source: null,
             title: 'Search',
-            value: { label: '', value: '' },
+            value: null,
             width: 600
         },
         _create: function () {
 
-            var $this = this;
-
-            if ($this.options.name == null) {
-                $this.options.name = 'dialog_' + Math.floor(Math.random() * 10001);
+            if (this.options.name === null) {
+                this.options.name = 'dialog_' + Math.floor(Math.random() * 10001).toString();
             }
 
-            var dialogBody = $.lookup.createBodyTemplate($this.options.name);
+            var $this = this,
+				dialogBody = $.lookup.createBodyTemplate($this.options.name),
+				buttons = { };
 
-            var buttons = { };
-            buttons[$this.options.okText] = function() {
+            buttons[$this.options.okText] = function () {
                 $this.options.value = $this.options._value;
                 if ($this.options.select) {
                     $this.options.select($this.options.value);
                 }
                 $(this).dialog("close");
             };
-            buttons[$this.options.cancelText] = function() {
+            buttons[$this.options.cancelText] = function () {
                 $(this).dialog("close");
             };
 
-            $this.options.dialog = $(dialogBody).dialog({
+            $this._dialog = $(dialogBody).dialog({
                 autoOpen: false,
                 height: $this.options.height,
                 modal: true,
@@ -68,52 +67,52 @@
                 width: $this.options.width,
                 buttons: buttons,
                 open: function (dialogEvent, dialogUi) {
-                    $('#' + $this.options.name + ' input')
-                    .autocomplete({
-                        appendTo: '#' + $this.options.name + ' .ui-lookup-results',
-                        delay: $this.options.delay,
-                        minLength: $this.options.minLength,
-                        source: $this.options.source,
-                        open: function (event, ui) {
-                            $('#' + $this.options.name + ' .ui-lookup-results ul').css({
-                                top: '',
-                                left: '',
-                                width: $('#' + $this.options.name + ' .ui-lookup-results').width(),
-                                height: $('#' + $this.options.name).height() - $('#' + $this.options.name + ' div:first').height(),
-                                overflowY: 'auto',
-                                overflowX: 'hidden'
-                            });
-                        },
-                        close: function (event, ui) {
-                            setTimeout(function () {
-                                $('#' + $this.options.name + ' .ui-lookup-results ul').show();
-                            }, 10);
-                            return false;
-                        },
-                        select: function (event, ui) {
-                            var selected = $('#ui-active-menuitem');
-                            setTimeout(function () {
-                                $this.options.dialog.find('.ui-lookup-results ul li a').removeClass('ui-state-active');
-                                selected.addClass('ui-state-active');
-                            }, 10);
-                            $this.options._value = ui.item;
-                            return false;
-                        },
-                        focus: function (event, ui) {
-                            var focused = $('#ui-active-menuitem');
-                            if (!focused.hasClass('ui-state-active')) {
-                                focused.addClass('ui-state-hover');
-                            }
-                            return false;
-                        }
-                    })
-                    .focus();
+                    $this._autocomplete = $('input', $this._dialog)
+						.autocomplete({
+							appendTo: '#' + $this.options.name + ' .ui-lookup-results',
+							delay: $this.options.delay,
+							minLength: $this.options.minLength,
+							source: $this.options.source,
+							open: function (event, ui) {
+								$('.ui-lookup-results ul', $this._dialog).css({
+									top: '',
+									left: '',
+									width: $('.ui-lookup-results', $this._dialog).width(),
+									height: $this._dialog.height() - $('div:first', $this._dialog).height(),
+									overflowY: 'auto',
+									overflowX: 'hidden'
+								});
+							},
+							close: function (event, ui) {
+								setTimeout(function () {
+									$('#' + $this.options.name + ' .ui-lookup-results ul').show();
+								}, 10);
+								return false;
+							},
+							select: function (event, ui) {
+								var selected = $('#ui-active-menuitem');
+								setTimeout(function () {
+									$this._dialog.find('.ui-lookup-results ul li a').removeClass('ui-state-active');
+									selected.addClass('ui-state-active');
+								}, 10);
+								$this.options._value = ui.item;
+								return false;
+							},
+							focus: function (event, ui) {
+								var focused = $('#ui-active-menuitem');
+								if (!focused.hasClass('ui-state-active')) {
+									focused.addClass('ui-state-hover');
+								}
+								return false;
+							}
+						})
+						.focus();
                 }
             });
 
             $this.element.bind('focus', function () {
                 if (!$this.options.disabled) {
-                    $('#' + $this.options.name).dialog('open');
+                    $this._dialog.dialog('open');
                 }
             });
         },
@@ -121,7 +120,8 @@
             return this.options.value;
         },
         destroy: function () {
-            this.options.dialog.dialog('destroy');
+			this._autocomplete.autocomplete('destroy');
+            this._dialog.dialog('destroy');
             $.Widget.prototype.destroy.apply(this, arguments);
         },
         disable: function () {
@@ -132,4 +132,4 @@
         }
     });
 
-} (jQuery));
+}(jQuery));
